e5015f9641f5a68c3003165a8e08a46329d1d1a0
zhangbin <i@zhangbin.cc>
2014-05-29 22:53:56 +0800

    add screenshots

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 RWBarChartView
 ==============
+
+A scrollable, highly customizable and easy to use charting view for bar graphs. Introduced in [China Air Quality Index](http://air.fresh-ideas.cc).
+
+
+## Features
+
+## Demo
+
+## Installation
+
+## Usage
+
+## License
+MIT License
--- RWBarChartViewDemo/RWBarChartViewDemo/RWAppDelegate.m
+++ RWBarChartViewDemo/RWBarChartViewDemo/RWAppDelegate.m
@@ -14,6 +14,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    srandomdev();
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor blackColor];
     
